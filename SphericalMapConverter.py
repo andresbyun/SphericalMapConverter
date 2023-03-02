627c5189bca91c2225c284cff89d0c561132229c
Andres Byun <adb0202@hotmail.com>
2023-03-02 15:08:32 -0800

    Update requirements.txt

--- SphericalMapConverter.py
+++ SphericalMapConverter.py
@@ -10,5 +10,6 @@ if __name__ == "__main__":
     # TODO: Process arguments and then send to main
     # for now just focus on input and output flags
     parser = argparse.ArgumentParser(description='Transform input image into Equirectangular/Spherical projection')
+    parser.add_argument('input', help='input filename', metavar='input', required=True)
 
     main(sys.argv)
